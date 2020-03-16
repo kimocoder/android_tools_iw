902de867453f63ca4f7b7695b48f0250c616540e
Christian Bremvåg <christian@aircrack-ng.org>
2020-03-16 22:23:03 +0100

    Add files via upload

---
+++ android_buildenv.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+NDK="/root/Android/Sdk/ndk/android-ndk-r11c"
+CC_DIR="$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin"
+SYSROOT="$NDK/platforms/android-21/arch-arm"
+
+CROSS_COMPILE="$CC_DIR/arm-linux-androideabi-"
+CC="${CROSS_COMPILE}gcc"
+CXX="${CROSS_COMPILE}g++"
+LD="${CROSS_COMPILE}ld"
+
+CFLAGS="-Os -ggdb -fPIE --sysroot=$SYSROOT"
+CXXFLAGS="$CFLAGS"
+CPPFLAGS="--sysroot=$SYSROOT"
+LDFLAGS="--sysroot=$SYSROOT -pie"
+
+export CROSS_COMPILE CC CXX LD CPPFLAGS CFLAGS CXXFLAGS LDFLAGS
+exec "$@"
+
